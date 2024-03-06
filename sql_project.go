9d837e583ec2a54497f134e0c15a39c54fb1073f
mycaelli.lima <mycaelli.lima@g.globo>
2024-03-06 08:55:26 -0300

    chore: sql project with transactions

--- sql_project.go
+++ sql_project.go
@@ -14,25 +14,29 @@ func main() {
 	}
 	defer db.Close()
 
-	err = db.Ping()
+	tx, err := db.Begin()
 	if err != nil {
-		log.Fatal("zzZZZzz", err)
+		log.Fatal(err)
 	}
-	
-	stmt, err := db.Prepare("CREATE DATABASE IF NOT EXISTS hello")
 
+	stmt, err := tx.Prepare("CREATE DATABASE IF NOT EXISTS hello")
 	if err != nil {
 		log.Fatal("Erro na preparação da query", err)
 	}
-	
+
 	_, err = stmt.Exec()
 	if err != nil {
 		log.Fatal("Erro ao criar o banco de dados: ", err)
 	}
 
 	defer stmt.Close()
-	
-	stmt, err = db.Prepare(`CREATE TABLE IF NOT EXISTS users (
+
+	_, err = tx.Exec("USE hello")
+	if err != nil {
+		log.Fatal("Erro ao selecionar o banco de dados: ", err)
+	}
+
+	stmt, err = tx.Prepare(`CREATE TABLE IF NOT EXISTS users (
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		nome VARCHAR(50),
 		idade INT)`)
@@ -47,12 +51,7 @@ func main() {
 		log.Fatal("Erro ao criar tabela:", err)
 	}
 
-	_, err = db.Exec("USE hello")
-	if err != nil {
-		log.Fatal("Erro ao selecionar o banco de dados: ", err)
-	}
-
-	stmt, err = db.Prepare(`INSERT INTO users (nome, idade) VALUES (?, ?)`)
+	stmt, err = tx.Prepare(`INSERT INTO users (nome, idade) VALUES (?, ?)`)
 	if err != nil {
 		log.Fatal("Erro na preparação da query", err)
 	}
@@ -63,9 +62,9 @@ func main() {
 		nome string
 		idade int
 	}{
-		{"Carol", 34},
-		{"Ana", 29},
-		{"Maria", 41},
+		{"Natalia", 25},
+		{"Daniel", 39},
+		{"Junior", 48},
 	}
 
 	for _, usuario := range usuarios {
@@ -88,7 +87,7 @@ func main() {
 		id int
 		nome string
 	)
-	stmt, err = db.Prepare("SELECT id, nome FROM users")
+	stmt, err = tx.Prepare("SELECT id, nome FROM users")
 	if err != nil {
 		log.Fatal(err)
 	}
